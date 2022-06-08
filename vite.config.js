3657445b8b09c473ef616d0a877a2e69c54f9c96
ydcjeff <32727188+ydcjeff@users.noreply.github.com>
2022-06-08 12:36:28 +0630

    chore: setup alias in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '~': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
